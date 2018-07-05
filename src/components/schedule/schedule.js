13b817834eafeed316ccd938a7d0d6376e38b911
Miguel Rust <hava12soft@gmail.com>
2018-07-05 08:21:12 -0600

    Built gradient.

--- src/components/schedule/schedule.js
+++ src/components/schedule/schedule.js
@@ -1,10 +1,13 @@
 import React, {Component} from "react";
-
+import Gradient from "../gradient";
 class Schedule extends Component {
   render(){
     return(
-      <div className="schedule">Schedule</div>
-    )
+      <div className="schedule">
+        <div>Schedule</div>
+        <Gradient />
+      </div>
+    );
   }
 }
 export default Schedule;
