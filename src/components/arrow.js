07d03f3d33d1ae410b71d3acd8a7c57f2b6c170b
Miguel Rust <hava12soft@gmail.com>
2018-07-09 09:45:01 -0600

    Added course selected styles.

--- src/components/arrow.js
+++ src/components/arrow.js
@@ -1,8 +1,17 @@
 import React, {Component} from "react";
 class Arrow extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      style: "arrow"
+    }
+  }
+  condition = function() {
+    return (this.props.open) ? "arrow" : "arrow-closed";
+  }.bind(this);
   render(){
       return(
-        <div className={`${this.props.className} arrow`}></div>
+        <a className={`${this.props.className} ${this.condition()}`} onClick={() => this.props.onClick()}></a>
       );
   }
 }
--- src/components/library/libraryCourse.js
+++ src/components/library/libraryCourse.js
@@ -5,20 +5,39 @@ import Arrow from "../arrow";
 import Action from "../action";
 import * as actions from "../../actions";
 class LibraryCourse extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      open: false
+    }
+  }
+  toggle = function(){
+    console.log("toggling");
+    console.log(this.state.open);
+    const newOpen = (this.state.open) ? false : true
+    this.setState({
+      open: newOpen
+    });
+  }.bind(this);
+  isSolid = function(){
+    return (this.state.open) ? "library-course-selected" : "";
+  }.bind(this);
   render(){
     return(
-      <div className="library-course">
+      <div className={`library-course ${this.isSolid()}`}>
         <div className="library-course__title-check">
           <label className="library-course__title">{this.props.title}</label>
           { Icon("fas fa-check", "library-course__icon") }
         </div>
-        <div className="library-coures__line"></div>
-        <Arrow className="library-course__arrow"/>
-        <Action onClick={() => this.props.toggleEnrolled(this.props.id)}/>
+        <div className="library-course__line"></div>
+        <Arrow className="library-course__arrow" onClick={this.toggle} open={this.state.open}/>
+        <Action onClick={() => {this.props.toggleEnrolled(this.props.id); this.setState({open: false});}}/>
+        { this.state.open &&
         <div className="libary-course__description">
           <label>Course Description</label>
           <p>{this.props.description}</p>
         </div>
+        }
       </div>
     );
   }
