35f6fd071b0ef10eeaf16dd0806a9d0bb44780ba
Miguel Rust <hava12soft@gmail.com>
2018-07-09 10:51:30 -0600

    Added animation to card expand.

--- src/components/library/libraryCourse.js
+++ src/components/library/libraryCourse.js
@@ -1,9 +1,11 @@
 import React, {Component} from "react";
 import {connect} from "react-redux";
+import AnimateHeight from 'react-animate-height';
 import Icon from "../icon";
 import Arrow from "../arrow";
 import Action from "../action";
 import * as actions from "../../actions";
+
 class LibraryCourse extends Component{
   constructor(props){
     super(props);
@@ -32,12 +34,12 @@ class LibraryCourse extends Component{
         <div className="library-course__line"></div>
         <Arrow className="library-course__arrow" onClick={this.toggle} open={this.state.open}/>
         <Action onClick={() => {this.props.toggleEnrolled(this.props.id); this.setState({open: false});}} open={this.props.enrolled}/>
-        { this.state.open &&
-        <div className="libary-course__description">
-          <label>Course Description</label>
-          <p>{this.props.description}</p>
-        </div>
-        }
+        <AnimateHeight duration="300" height={(this.state.open) ? "auto" : 0}>
+          <div className="libary-course__description">
+            <label>Course Description</label>
+            <p>{this.props.description}</p>
+          </div>
+        </AnimateHeight>
       </div>
     );
   }
