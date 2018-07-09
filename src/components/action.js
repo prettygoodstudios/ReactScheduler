50e5f55efec3c9f48ef1f28ff339d2662340ad52
Miguel Rust <hava12soft@gmail.com>
2018-07-09 10:25:44 -0600

    Built out add remove button.

--- src/components/action.js
+++ src/components/action.js
@@ -1,11 +1,16 @@
 import React, {Component} from "react";
 import Icon from "./icon";
 class Action extends Component{
+  handleAction = function(){
+    this.props.onClick();
+
+    //document.getElementByID("action").
+  }.bind(this);
   render(){
       return(
-        <button onClick={() => this.props.onClick() }  className="library-course__action">
+        <a id="action" onClick={ () => this.handleAction() }  className="library-course__action">
           {Icon("fas fa-plus-circle","library-course__action")}
-        </button>
+        </a>
       );
   }
 }
